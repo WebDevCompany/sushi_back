838aae64887610982e99e34b114991de29e3df76
AlexShvet <malifey39@gmail.com>
2022-07-26 21:53:26 +0700

    создание миграции таблицы user

--- database/migrations/2022_07_26_143236_create_user_table.php
+++ database/migrations/2022_07_26_143236_create_user_table.php
@@ -13,14 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name');
+            $table->string('city');
+            $table->string('phone');
+            $table->string('reviews');
+            $table->string('number_of_visits');
+            $table->string('number_of_paid_orders');
+            $table->string('promo_code');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('user');
     }
 };
