1ad4caeacb07ca299f6009fec0e4cbb0e124e4b4
aaxpost <aaxpost@gmail.com>
2022-08-08 23:04:52 +0300

    В прогрессе переноса элементов админки в компоненты

---
+++ resources/views/components/dashboard/brend-logo.blade.php
@@ -0,0 +1,7 @@
+<brend-logo>
+    <a href="index3.html" class="brand-link">
+        <img src="{{ URL::asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">AdminSushiSite</span>
+    </a>
+<brend-logo/>
+
