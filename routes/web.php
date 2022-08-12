f9d8116047c356a7737ff01ad385da0431f7f3c4
aaxpost <aaxpost@gmail.com>
2022-08-12 22:45:32 +0300

    Revert "Начал разработку админской части."
    This reverts commit d335fbde10f34290d21fedad6a4a95ad127f0f5e.

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Http\Controllers\Dashboard\HomeDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,82 +13,12 @@ use App\Http\Controllers\Dashboard\HomeDashboardController;
 |
 */
 
-/*
-|Роуты панели управления
- */
-
-Route::redirect('/admin', '/login');
-
 Route::get('/', function () {
-    return view('layouts.layout');
+    return view('welcome');
 });
 
-
-
-Route::get('/dashboard', [HomeDashboardController::class, 'show'])
-        ->middleware(['auth'])
-        ->name('dashboard');
-
-Route::get('/dashboard/category', [CategoryDashboardController::class, 'show'])
-        ->middleware(['auth'])
-        ->name('dashboard-category');
-
-Route::get('/dashboard/product', [ProductDashboardController::class, 'show-all'])
-        ->middleware(['auth'])
-        ->name('dashboard-product');
-
-Route::get('/dashboard/product/{slug}', [ProductDashboardController::class, 'show-one'])
-        ->middleware(['auth'])
-        ->name('dashboard-product-slug');
-
-Route::get('/dashboard/review', [ReviewDashboardController::class, 'show-all'])
-        ->middleware(['auth'])
-        ->name('dashboard-review');
-
-
-
-/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
-*/
 
 require __DIR__.'/auth.php';
-
-
-/* Роуты пользовательской части сайта */
-/*
-Route::get('/', UserController::class . '@show');
-
-Route::get('/', function () {
-    return view('test');
-});
-*/
-
-Route::get('/', function () {
-    return view('layouts.layout');
-});
-
-Route::get('/name_category', function () {
-    return "Категории";
-});
-
-Route::get('/name_product', function () {
-    return "Продукт";
-});
-
-Route::get('/reviews/{id}', ReviewsController::class . '@show');
-
-
-Route::get('/order', function () {
-    return "Оформление заказа";
-});
-
-Route::get('/order1', function () {
-    return "Оформление заказа1";
-});
-
-Route::get('/order2', function () {
-    return "Оформление заказа2";
-});
-
